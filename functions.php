8f881a3b527d25c45fabf648c0c8fb5aac1fe51b
Andy Place <info@andyplace.co.uk>
2014-06-15 20:56:19 +0200

    Renaming function to be more consistent

--- functions.php
+++ functions.php
@@ -3,10 +3,10 @@
 /**
  * Enqueue style sheet, as parent Kayla theme doesn't use style.css
  */
-function mnm_kayla_enqueue_styles() {
+function mnm_kayla_print_styles() {
   wp_enqueue_style( 'mnm-kayla-style', get_stylesheet_uri() );
 }
-add_action('wp_print_styles', 'mnm_kayla_enqueue_styles', 50);
+add_action('wp_print_styles', 'mnm_kayla_print_styles', 50);
 
 /**
  * Based on ticket availability show themed statement to suit
@@ -20,3 +20,12 @@ function mnm_kayla_show_ticket_availability( $EM_Ticket ) {
     return '<span class="spaces-sold-out">Sold out</span>';
   }
 }
+
+/**
+ * Hook into booking form submit button so we can add out own classes
+ */
+function mnm_kayla_em_booking_form_buttons($input) {
+  die('POW!');
+  return '<input type="submit" class="em-booking-submit wt_button small white" value="Submit" />';
+}
+add_action('em_booking_form_buttons', 'mnm_kayla_em_booking_form_buttons', 10, 1);
