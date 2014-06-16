c32f6b03d41fddaaa795503875900d98640df653
Andy Place <info@andyplace.co.uk>
2014-06-16 11:06:52 +0200

    Move login link to be above the booking form Need to do this via jquery as once again EM gives us no control over how the booking form and login is churned out.

--- js/kayla-child.js
+++ js/kayla-child.js
@@ -1,8 +1,14 @@
 jQuery(document).ready(function($){
   
+  // Style booking form button
   var bookingFormButton = $('#em-booking-submit');
-
   if( bookingFormButton.length ) {
     bookingFormButton.addClass('wt_button dark_green');
+  }
+
+  // Reposition login form links above booking form
+  var bookingLogin = $('.em-booking-login');
+  if( bookingLogin.length ) {
+    bookingLogin.insertBefore( $('form.em-booking-form') );
   }
 });
