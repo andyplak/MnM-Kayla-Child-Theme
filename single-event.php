44d012505a55029fc9995d1bd76f409acc399634
Andy Place <info@andyplace.co.uk>
2014-07-01 08:44:17 +0100

    Using icon on event detail page

--- single-event.php
+++ single-event.php
@@ -51,11 +51,11 @@ ID, 'post_id' );
                 echo '<div class="one_half '.$last.'">';
 
                 if( $EM_Ticket->ticket_name == 'Male' ) {
-                  echo "<h5>♂ Men</h5>";
+                  echo '<h5><i class="mnm-icon male"></i> Men</h5>';
                   echo "Age: ".$EM_Event->event_attributes['Male Age Range'];
                 }
                 if( $EM_Ticket->ticket_name == 'Female' ) {
-                  echo "<h5>♀ Women</h5>";
+                  echo '<h5><i class="mnm-icon female"></i> Women</h5>';
                   echo "Age: ".$EM_Event->event_attributes['Female Age Range'];
                 }
                 echo '<br />', mnm_kayla_show_ticket_availability( $EM_Ticket ), '</div>';
