cce88ccee318a64ce05848fbb4e7cc3c4903c164
Andy Place <info@andyplace.co.uk>
2014-07-03 09:20:08 +0100

    Only show address lines if we have data

--- single-location.php
+++ single-location.php
@@ -80,12 +80,12 @@
         <div class="framed_box_content clearfix">
           <h5><i class="wt-icon-map-marker"></i> Address</h5>
           <p>
-            <?php echo $EM_Location->output('#_LOCATIONNAME') ?><br />
-            <?php echo $EM_Location->output('#_LOCATIONADDRESS') ?><br />
-            <?php echo $EM_Location->output('#_LOCATIONTOWN') ?><br />
-            <?php echo $EM_Location->output('#_LOCATIONREGION') ?>,
-            <?php echo $EM_Location->output('#_LOCATIONPOSTCODE') ?><br />
-            <?php echo $EM_Location->output('#_LOCATIONCOUNTRY') ?><br />
+            <?php echo ( $EM_Location->output('#_LOCATIONNAME') != "" ? $EM_Location->output('#_LOCATIONNAME') .'<br />' : "" ) ?>
+            <?php echo ( $EM_Location->output('#_LOCATIONADDRESS') != "" ? $EM_Location->output('#_LOCATIONADDRESS') .'<br />' : "" ) ?>
+            <?php echo ( $EM_Location->output('#_LOCATIONTOWN') != "" ? $EM_Location->output('#_LOCATIONTOWN') .'<br />' : "" ) ?>
+            <?php echo ( $EM_Location->output('#_LOCATIONREGION') != "" ? $EM_Location->output('#_LOCATIONREGION') .',' : "" ) ?>
+            <?php echo ( $EM_Location->output('#_LOCATIONPOSTCODE') != "" ? $EM_Location->output('#_LOCATIONPOSTCODE') .'<br />' : "" ) ?>
+            <?php echo ( $EM_Location->output('#_LOCATIONCOUNTRY') != "" ? $EM_Location->output('#_LOCATIONCOUNTRY') .'<br />' : "" ) ?>
 
             <?php
               $directions_url = "http://maps.google.com.au/maps?daddr=";
